a989aee049d132a45c3fa0633ca5cc08d6b4c5bc
Cristian Blanco <ccamiloblanco@gmail.com>
2025-08-21 07:38:08 -0500

    Routes eliminated until use it

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,39 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
-    {
-        path: "/",
-        component: () => import("@/layouts/DashboardLayout.vue"),
-        children: [
-            {
-                path: "",
-                name: "Dashboard",
-                component: () => import("@/pages/Dashboard.vue"),
-            },
-            {
-                path: "tickets",
-                name: "Tickets",
-                component: () => import("@/pages/Tickets.vue"),
-            },
-            {
-                path: "tickets/:id",
-                name: "TicketDetail",
-                component: () => import("@/pages/TicketDetail.vue"),
-                props: true,
-            },
-        ],
-    },
-    {
-        path: "/auth",
-        component: () => import("@/layouts/AuthLayout.vue"),
-        children: [
-            {
-                path: "login",
-                name: "Login",
-                component: () => import("@/pages/Login.vue"),
-            },
-        ],
-    },
 ];
 
 const router = createRouter({
